e033028c8466cbf3185c73137a85ea30fa1d9bc0
ingriid-ayaala <ingriidayaala@gmail.com>
2022-09-27 22:47:51 +0200

    convertir de String a int
    en proceso

--- conversionTipo/src/conversiontipo/ConversionTipo.java
+++ conversionTipo/src/conversiontipo/ConversionTipo.java
@@ -4,17 +4,26 @@
  */
 package conversiontipo;
 
-/**
- *
- * @author ingri
- */
-public class ConversionTipo {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class ConversionTipo {
+static Scanner dato = new Scanner(System.in);
     public static void main(String[] args) {
         // TODO code application logic here
+        int num1 = 5;
+        
+        System.out.println("\nIntroduce un numero: \n");
+        String cad = dato.next();
+        int num = Integer.parseInt(cad);
+        System.out.println(cad);
+        System.out.println("la suma es"+(num1+num));
+        
+        //String cad="10",guardoEnt; 
+        //dato = Integer.parseInt(cad); // ya convertimos en entero la cadena
+        //System.out.println("Valores de las variables\n" + "ent=" +ent+"\nguardoCad="+guardoCad);
+        //System.out.println("Test---->"+(guardoCad+ent));
+        
     }
     
 }
